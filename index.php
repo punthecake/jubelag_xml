580642b5c77c99ce743c24ca74d324d9e5f242b2
punthecake <60122576+punthecake@users.noreply.github.com>
2020-01-28 11:38:03 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+ $domOBJ = new DOMDocument();
+ $domOBJ->load("https://jubelagxml.herokuapp.com/rss.php");//XML page URL
+ 
+ $content = $domOBJ->getElementsByTagName("ballpen");
+ 
+ ?>
+ <ul>
+   <?php
+ foreach( $content as $data )
+ {
+   $name = $data->getElementsByTagName("name")->item(0)->nodeValue;
+   $price = $data->getElementsByTagName("price")->item(0)->nodeValue;
+   $color = $data->getElementsByTagName("color")->item(0)->nodeValue;
+   $supplier = $data->getElementsByTagName("supplier")->item(0)->nodeValue;
+   echo "<li>$name
+            <ul>
+                <li>$price</li>
+                <li>$color</li>
+                <li>$supplier</li>
+            </ul>
+        </li>";
+ }
+?>
+</ul>
