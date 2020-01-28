ab4b49165970c6d2df633c6257b1bd2681ec9fa5
punthecake <60122576+punthecake@users.noreply.github.com>
2020-01-28 11:28:36 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,9 +9,10 @@
     <?php
  foreach( $content as $data )
  {
-   $title = $data->getElementsByTagName("ballpen_name")->item(0)->nodeValue;
-   $link = $data->getElementsByTagName("price")->item(0)->nodeValue;
-  
+   $name = $data->getElementsByTagName("ballpen_name")->item(0)->nodeValue;
+   $price = $data->getElementsByTagName("price")->item(0)->nodeValue;
+   $color = $data->getElementsByTagName("color")->item(0)->nodeValue;
+   $supplier = $data->getElementsByTagName("supplier")->item(0)->nodeValue;
    echo "<li>$ballpen_name
             <ul>
                 <li>$price</li>
