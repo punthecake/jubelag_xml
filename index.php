5a56b5df5a2a6575e7e46d9753a123b71794232a
punthecake <60122576+punthecake@users.noreply.github.com>
2020-02-07 21:42:52 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,19 +5,21 @@
  $content = $domOBJ->getElementsByTagName("ballpen");
  
  ?>
+<h2> Ballpens </h2>
  <ul>
-   <?php
+    <?php
  foreach( $content as $data )
  {
    $name = $data->getElementsByTagName("name")->item(0)->nodeValue;
    $price = $data->getElementsByTagName("price")->item(0)->nodeValue;
    $color = $data->getElementsByTagName("color")->item(0)->nodeValue;
    $supplier = $data->getElementsByTagName("supplier")->item(0)->nodeValue;
-   echo "<li>$name
+  
+   echo "<li><b>Ballpen:</b> $name
             <ul>
-                <li>$price</li>
-                <li>$color</li>
-                <li>$supplier</li>
+                <li> <b>Price:</b> $price</li>
+                <li> <b>Color:</b> $color</li>
+                <li> <b>Supplier:</b> $supplier</li>
             </ul>
         </li>";
  }
