cbb03743178d4a6db44f94cd2939f76a282bdfdd
punthecake <60122576+punthecake@users.noreply.github.com>
2020-02-07 21:54:11 +0800

    Update rss.php

--- rss.php
+++ rss.php
@@ -4,7 +4,7 @@ $rss= '<?xml version="1.0" encoding="UTF-8"?>';
 $rss .= '<rss version="2.0">';
 $rss .= '<channel>';
 
-$connect = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234", "db_1820843") or die (mysqli_error($connect));
+$connect = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234", "db_1820860") or die (mysqli_error($connect));
 $sql = "SELECT * FROM tblballpen";
 $query = mysqli_query($connect,$sql) or die (mysqli_error($connect));
 
